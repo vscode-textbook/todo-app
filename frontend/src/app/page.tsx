a5e847334e9ea3370a94b40ab2fbd84df04f6758
Yoichi Kawasaki <yokawasa@gmail.com>
2024-01-28 11:56:22 +0900

    commented disabling-ssr-on-specific-components
    Signed-off-by: Yoichi Kawasaki <yokawasa@gmail.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
-// import TodoList from "../components/TodoList";
+import TodoList from "../components/TodoList";
 // Disabling SSR on TodoList component
 // https://nextjs.org/docs/messages/react-hydration-error#solution-2-disabling-ssr-on-specific-components
-import dynamic from 'next/dynamic'
-const TodoList = dynamic(() => import('../components/TodoList'), { ssr: false })
+// import dynamic from 'next/dynamic'
+// const TodoList = dynamic(() => import('../components/TodoList'), { ssr: false })
 
 export default function Page() {
   return (
